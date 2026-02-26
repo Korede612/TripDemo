6ea80eb0f73e98ac485c3d7c3a383a8c1222ed93
Oko-Osi Korede <korede.oko-osi@fcmb.com>
2026-02-26 15:35:00 +0100

    Initial Commit

---
+++ GoPaddiAssessment/GoPaddiAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoPaddiAssessmentApp.swift
+//  GoPaddiAssessment
+//
+//  Created by Oko-osi Korede Ibrahim on 26/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct GoPaddiAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
